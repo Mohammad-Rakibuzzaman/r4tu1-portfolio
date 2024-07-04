47f21a5fd0b12a1918837082f50da5afa3068fd6
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-07-04 14:41:54 +0600

    feat: landing first page decorating

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+import Content from "./Layout/Content/Content";
+import Nav from "./Layout/Nav/Nav";
+import Side from "./Layout/Side/Side";
+
 const App: React.FC = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-yellow-100">
-      <h1 className="text-4xl font-bold text-blue-500">Hello Ratul</h1>
+    // <div className="flex items-center justify-center min-h-screen bg-white-100">
+    //   <h1 className="text-4xl font-bold text-blue-500">Hello Ratul</h1>
+    // </div>
+    <div className="grid grid-cols-12 gap-2 w-full h-full bg-white-600">
+      <Nav />
+      <Content />
+      <Side />
     </div>
   );
 };
---
+++ src/Layout/Content/Content.tsx
@@ -0,0 +1,12 @@
+const Content: React.FC = () => {
+  return (
+    <div
+      id="content"
+      className="col-span-7 bg-white-600 h-[calc(80vh-3.75rem)] p-4 "
+    >
+      Content Part
+    </div>
+  );
+};
+
+export default Content;
