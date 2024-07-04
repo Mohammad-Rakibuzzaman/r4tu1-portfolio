a7016b707941df6feace6fae0ea8712ec9f2992f
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-07-04 10:09:45 +0600

    feat: talwind css setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-yellow-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello Ratul</h1>
+    </div>
+  );
+};
+
+export default App;
